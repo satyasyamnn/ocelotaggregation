696ffd2c9e73fada02f25f781f63b4a8b45c8423
satyasyamnn <satyasyamnn@gmail.com>
2020-05-08 12:36:45 +0530

    Gateway

---
+++ Gateway/DataAggregator.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Ocelot.Middleware;
+using Ocelot.Middleware.Multiplexer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Gateway
+{
+    public class DataAggregator : IDefinedAggregator
+    {
+        public async Task<DownstreamResponse> Aggregate(List<DownstreamContext> responses)
+        {
+            IList<string> allResponses = new List<string>();
+            string message = string.Empty;
+            foreach (DownstreamContext context in responses)
+            {
+                string v = await context.DownstreamResponse.Content.ReadAsStringAsync();
+                object obj =  JsonConvert.DeserializeObject(v);         
+                message += await context.DownstreamResponse.Content.ReadAsStringAsync();
+            }            
+
+            var headers = responses.SelectMany(x => x.DownstreamResponse.Headers).ToList();
+            DownstreamResponse response = new DownstreamResponse(new StringContent(message), System.Net.HttpStatusCode.OK, headers, "Data Integrated");
+            return response;
+        }
+    }
+}
---
+++ Gateway/Program.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConsole();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("ocelot.json").AddEnvironmentVariables();
+                });
+    }
+}
